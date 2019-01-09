1ea11798c166437ad7d91d2b0ac602e7845383a4
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-01-09 18:22:49 +0200

    practise1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number:");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Power2(ref x);
+			Console.WriteLine($"power 2 of your number is {x}");
+		}
+		private static int Power2(ref int i)
+		{
+			i = i * 2;
+			return i;
+		}	
+	}
+}
