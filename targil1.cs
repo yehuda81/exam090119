adfb8d21a7749894583b215a3bb18ad2853a0ea4
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-01-09 21:34:20 +0200

    Add files via upload

---
+++ targil1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number:");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Power2(ref x);
+			Console.WriteLine($"power 2 of your number is {x}");
+		}
+		private static void Power2(ref int i)
+		{
+			i = i * 2;
+		}
+	}
+}
