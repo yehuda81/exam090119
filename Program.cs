11494c4993f8c06e128b41e5de8f0107bcac2ada
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-01-09 18:44:02 +0200

    practise4

--- Program.cs
+++ Program.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practise1
+namespace practise4
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Enter a number:");
-			int x = Convert.ToInt32(Console.ReadLine());
-			Power2(ref x);
-			Console.WriteLine($"power 2 of your number is {x}");
+			int[] a, b;
+			InitializeArrays(out a, out b);
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.WriteLine(a[i]);
+			}
+			for (int i = 0; i < b.Length; i++)
+			{
+				Console.WriteLine(b[i]);
+			}
 		}
-		private static int Power2(ref int i)
+		private static void InitializeArrays(out int[] a, out int[] b)
 		{
-			i = i * 2;
-			return i;
-		}	
+			Random r = new Random();
+			a = new int[10];
+			b = new int[10];
+			for (int i = 0; i < 10; i++)
+			{
+				a[i] = r.Next(101);
+				b[i] = r.Next(101);
+			}
+		}
 	}
 }
