75d6909b1aa9c6eb20b7c3f5fbdd9e15514b90c4
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-01-09 21:28:26 +0200

    חלק ב' תרגיל 1

--- Program.cs
+++ Program.cs
@@ -4,33 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practise4
+namespace practise2_1
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int[] a, b;
-			InitializeArrays(out a, out b);
+			Random r = new Random();
+
+			int sizeA = r.Next(5, 10);
+			int sizeB = r.Next(5, 10);
+			int[] a = new int [sizeA];
+			int[] b = new int [sizeB];
+			buildarr(a,b);			
+			int result = 0;
+			Measure(a, b, ref result);
+			Console.WriteLine(result);
+
+		}
+
+		private static void buildarr(int[] a, int [] b)
+		{
+			Random n = new Random();
 			for (int i = 0; i < a.Length; i++)
 			{
-				Console.WriteLine(a[i]);
+				a[i] = n.Next(100);
+				Console.Write($"{a[i]}  ");
 			}
+			Console.WriteLine();
+			
 			for (int i = 0; i < b.Length; i++)
 			{
-				Console.WriteLine(b[i]);
+				b[i] = n.Next(100);
+				Console.Write($"{b[i]}  ");
 			}
+			Console.WriteLine();
 		}
-		private static void InitializeArrays(out int[] a, out int[] b)
+
+
+		private static int Measure(int[] a, int[] b, ref int result)
 		{
-			Random r = new Random();
-			a = new int[10];
-			b = new int[10];
-			for (int i = 0; i < 10; i++)
+			int l;
+			int sumA = 0;
+			int sumB = 0;
+			result = 0;
+			
+			if (a.Length > b.Length)
 			{
-				a[i] = r.Next(101);
-				b[i] = r.Next(101);
+				l = b.Length;
 			}
+			else
+			{
+				l = a.Length;
+			}
+			for (int i = 0; i < l; i++)
+			{
+				if (a[i] > b [i])
+				{
+					sumA++;
+				}
+				if (b[i] > a[i])
+				{
+					sumB++;
+				}
+
+			}
+			if (sumA > sumB)
+			{
+				result = 1;
+			}
+			if (sumB > sumA)
+			{
+				result = -1;
+			}
+			return result;
 		}
 	}
 }
