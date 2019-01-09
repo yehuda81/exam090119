40a6411cfe256bd5d6e912c65e77b71f376a2207
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-01-09 21:35:47 +0200

    Add files via upload

---
+++ targil4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] a, b;
+			InitializeArrays(out a, out b);
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.WriteLine(a[i]);
+			}
+			for (int i = 0; i < b.Length; i++)
+			{
+				Console.WriteLine(b[i]);
+			}
+		}
+		private static void InitializeArrays(out int[] a, out int[] b)
+		{
+			Random r = new Random();
+			a = new int[10];
+			b = new int[10];
+			for (int i = 0; i < 10; i++)
+			{
+				a[i] = r.Next(101);
+				b[i] = r.Next(101);
+			}
+		}
+	}
+}
